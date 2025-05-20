115882bd19bda38211ac64b931ad3bc81b78fee6
ori <oribarilan@gmail.com>
2025-05-20 10:05:12 +0300

    removing catppuccin and lsp-config

--- lua/plugins/catppuccin.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'catppuccin/nvim',
-  name = 'catppuccin',
-  priority = 1000,
-  config = function()
-    ---@diagnostic disable-next-line: missing-fields
-    require('catppuccin').setup()
-  end,
-}
