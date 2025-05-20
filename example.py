d3be5b7274f2cf8a5bf6c78f8bd48041aee07cbf
ori <oribarilan@gmail.com>
2025-05-20 15:41:15 +0300

    ruff python lint and format

---
+++ example.py
@@ -0,0 +1,11 @@
+
+
+def greet(name="World" ):
+    print(f"Hello, {name}!")
+def main():
+    greet("Alice")
+    greet()
+    greet("Bob")
+
+
+main()
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,7 +7,10 @@ return {
     local null_ls = require 'null-ls'
     null_ls.setup {
       sources = {
+        null_ls.builtins.formatting.prettier.with { filetypes = { 'json', 'yaml', 'markdown' } },
         null_ls.builtins.formatting.stylua,
+        require('none-ls.formatting.ruff').with { extra_args = { '--extend-select', 'I' } },
+        require 'none-ls.formatting.ruff_format',
       },
     }
     vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, {})
