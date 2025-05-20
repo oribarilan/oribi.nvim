380741502e45103f4cb7201ee134352cc560a79e
ori <oribarilan@gmail.com>
2025-05-20 09:54:41 +0300

    extract whickey and add comment

---
+++ lua/plugins/comment.lua
@@ -0,0 +1,7 @@
+-- use ggc (linewise) or gbc (blockwise) for comments, in both normal and visual mode
+-- dot repeatable
+-- can be used with motions (gco, gc}, etc.)
+return {
+  'numToStr/Comment.nvim',
+  opts = {},
+}
