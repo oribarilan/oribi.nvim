c0136f6efeb2030a913bccb468acac4b06f70ae6
ori <oribarilan@gmail.com>
2025-05-20 20:44:23 +0300

    fixing keymap

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -277,10 +277,10 @@ return {
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        'stylua',       -- Lua formatter
+        'stylua', -- Lua formatter
         'markdownlint', -- Markdown linter
-        'yamllint',     -- YAML linter
-        'jsonlint'      -- JSON linter
+        'yamllint', -- YAML linter
+        'jsonlint', -- JSON linter
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -301,6 +301,46 @@ return {
     end,
   },
 
+  { -- Autoformat
+    'stevearc/conform.nvim',
+    event = { 'BufWritePre' },
+    cmd = { 'ConformInfo' },
+    keys = {
+      {
+        '<leader>f',
+        function()
+          require('conform').format { async = true, lsp_format = 'fallback' }
+        end,
+        mode = '',
+        desc = '[F]ormat buffer',
+      },
+    },
+    opts = {
+      notify_on_error = false,
+      format_on_save = function(bufnr)
+        -- Disable "format_on_save lsp_fallback" for languages that don't
+        -- have a well standardized coding style. You can add additional
+        -- languages here or re-enable it for the disabled ones.
+        local disable_filetypes = { c = true, cpp = true }
+        if disable_filetypes[vim.bo[bufnr].filetype] then
+          return nil
+        else
+          return {
+            timeout_ms = 500,
+            lsp_format = 'fallback',
+          }
+        end
+      end,
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        -- Use Ruff's LSP formatter
+        python = { 'ruff' },
+        --
+        -- You can use 'stop_after_first' to run the first available formatter from the list
+        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+      },
+    },
+  },
 
   { -- Autocompletion
     'saghen/blink.cmp',
