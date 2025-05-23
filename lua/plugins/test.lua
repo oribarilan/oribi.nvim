9cc7474f399823ca133967fbb583cb765e793fbd
ori <oribarilan@gmail.com>
2025-05-23 22:33:16 +0300

    optimized test.lua to not load on startup

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -1,31 +1,3 @@
-vim.keymap.set('n', '<leader>tn', function()
-  require('neotest').run.run()
-end, { desc = 'Run nearest test' })
-
-vim.keymap.set('n', '<leader>tf', function()
-  require('neotest').run.run(vim.fn.expand '%')
-end, { desc = 'Run all tests in file' })
-
-vim.keymap.set('n', '<leader>ts', function()
-  require('neotest').summary.toggle()
-end, { desc = 'Toggle test summary' })
-
-vim.keymap.set('n', '<leader>to', function()
-  require('neotest').output.open { enter = true }
-end, { desc = 'Open test output' })
-
-vim.keymap.set('n', '<leader>td', function()
-  require('neotest').run.run { strategy = 'dap' }
-end, { desc = 'Debug nearest test' })
-
-vim.keymap.set('n', '<leader>tp', function()
-  require('neotest').output_panel.open { enter = true }
-end, { desc = 'Open and enter Neotest output panel' })
-
-vim.keymap.set('n', '<leader>ts', function()
-  require('neotest').summary.toggle()
-end, { desc = 'Toggle Neotest test summary' })
-
 return {
   'nvim-neotest/neotest',
   dependencies = {
@@ -34,6 +6,50 @@ return {
     'antoinemadec/FixCursorHold.nvim',
     'nvim-treesitter/nvim-treesitter',
     'nvim-neotest/neotest-python',
+  },
+  keys = {
+    {
+      '<leader>tn',
+      function()
+        require('neotest').run.run()
+      end,
+      desc = 'Run nearest test',
+    },
+    {
+      '<leader>tf',
+      function()
+        require('neotest').run.run(vim.fn.expand '%')
+      end,
+      desc = 'Run all tests in file',
+    },
+    {
+      '<leader>ts',
+      function()
+        require('neotest').summary.toggle()
+      end,
+      desc = 'Toggle test summary',
+    },
+    {
+      '<leader>to',
+      function()
+        require('neotest').output.open { enter = true }
+      end,
+      desc = 'Open test output',
+    },
+    {
+      '<leader>td',
+      function()
+        require('neotest').run.run { strategy = 'dap' }
+      end,
+      desc = 'Debug nearest test',
+    },
+    {
+      '<leader>tp',
+      function()
+        require('neotest').output_panel.open { enter = true }
+      end,
+      desc = 'Open output panel',
+    },
   },
   config = function()
     require('neotest').setup {
