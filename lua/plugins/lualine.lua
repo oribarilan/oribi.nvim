b77c374e3d84afc296cc5fd91372d282b7c434d6
ori <oribarilan@gmail.com>
2025-05-23 21:10:58 +0300

    adding macro recording to lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,10 +1,27 @@
 return {
   'nvim-lualine/lualine.nvim',
   config = function()
+    local function recording_macro()
+      local rec = vim.fn.reg_recording()
+      return rec ~= '' and 'Recording @' .. rec or ''
+    end
+
     require('lualine').setup {
       options = {
         theme = 'dracula',
       },
+      sections = {
+        lualine_c = {
+          'filename',
+          recording_macro,
+        },
+      },
     }
+
+    vim.api.nvim_create_autocmd({ 'RecordingEnter', 'RecordingLeave' }, {
+      callback = function()
+        require('lualine').refresh()
+      end,
+    })
   end,
 }
