2df13d8b5cbc2954c9de24b817e78fe802e6e93f
ori <oribarilan@gmail.com>
2025-05-25 21:52:38 +0300

    fixing formatting and linting (nvim.lint and conform instead of none-ls)

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -1,67 +1,62 @@
 return {
-
-    { -- Linting
-        'mfussenegger/nvim-lint',
-        event = { 'BufReadPre', 'BufNewFile' },
-        config = function()
-            local lint = require 'lint'
-            -- Disable default linters to avoid duplicates
-            lint.linters_by_ft = {}
-
-            -- Configure only our chosen linters
-            lint.linters_by_ft = {
-                markdown = { 'markdownlint' },
-                json = { 'jsonlint' },
-                yaml = { 'yamllint' },
-            }
-
-            -- To allow other plugins to add linters to require('lint').linters_by_ft,
-            -- instead set linters_by_ft like this:
-            -- lint.linters_by_ft = lint.linters_by_ft or {}
-            -- lint.linters_by_ft['markdown'] = { 'markdownlint' }
-            --
-            -- However, note that this will enable a set of default linters,
-            -- which will cause errors unless these tools are available:
-            -- {
-            --   clojure = { "clj-kondo" },
-            --   dockerfile = { "hadolint" },
-            --   inko = { "inko" },
-            --   janet = { "janet" },
-            --   json = { "jsonlint" },
-            --   markdown = { "vale" },
-            --   rst = { "vale" },
-            --   ruby = { "ruby" },
-            --   terraform = { "tflint" },
-            --   text = { "vale" }
-            -- }
-            --
-            -- You can disable the default linters by setting their filetypes to nil:
-            -- lint.linters_by_ft['clojure'] = nil
-            -- lint.linters_by_ft['dockerfile'] = nil
-            -- lint.linters_by_ft['inko'] = nil
-            -- lint.linters_by_ft['janet'] = nil
-            -- lint.linters_by_ft['json'] = nil
-            -- lint.linters_by_ft['markdown'] = nil
-            -- lint.linters_by_ft['rst'] = nil
-            -- lint.linters_by_ft['ruby'] = nil
-            -- lint.linters_by_ft['terraform'] = nil
-            -- lint.linters_by_ft['text'] = nil
-
-            -- Create autocommand which carries out the actual linting
-            -- on the specified events.
-            local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
-            vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
-                group = lint_augroup,
-                callback = function()
-                    -- Only run the linter in buffers that you can modify in order to
-                    -- avoid superfluous noise, notably within the handy LSP pop-ups that
-                    -- describe the hovered symbol using Markdown.
-                    if vim.bo.modifiable then
-                        lint.try_lint()
-                    end
-                end,
-            })
-            vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, {})
+  {
+    'stevearc/conform.nvim',
+    event = { 'BufWritePre' },
+    cmd = { 'ConformInfo' },
+    keys = {
+      {
+        '<leader>f',
+        function()
+          require('conform').format { async = true, lsp_fallback = true }
         end,
+        mode = '',
+        desc = 'Format buffer',
+      },
     },
+    opts = {
+      formatters_by_ft = {
+        -- brew install stylua
+        lua = { 'stylua' },
+        -- pip install ruff
+        python = { 'ruff_format' },
+        -- npm install -g prettier
+        json = { 'prettier' },
+        yaml = { 'prettier' },
+        markdown = { 'prettier' },
+      },
+      formatters = {
+        prettier = {
+          prepend_args = {
+            '--print-width',
+            '100',
+            '--prose-wrap',
+            'always',
+            '--use-tabs',
+            'false',
+            '--tab-width',
+            '2',
+          },
+        },
+        stylua = {
+          prepend_args = { '--indent-type', 'Spaces', '--indent-width', '2' },
+        },
+      },
+    },
+  },
+  {
+    'mfussenegger/nvim-lint',
+    event = { 'BufWritePost', 'BufReadPost', 'InsertLeave' },
+    config = function()
+      local lint = require 'lint'
+      lint.linters_by_ft = {
+        python = { 'ruff' },
+      }
+
+      vim.api.nvim_create_autocmd({ 'BufWritePost', 'BufReadPost', 'InsertLeave' }, {
+        callback = function()
+          require('lint').try_lint()
+        end,
+      })
+    end,
+  },
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,9 +2,9 @@
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.opt.expandtab = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
 
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
