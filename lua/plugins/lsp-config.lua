97948ca104bf361a0faddcdb7aaa0b6003c81cf2
ori <oribarilan@gmail.com>
2025-07-03 17:46:51 +0300

    conform in lint.lua only

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -340,50 +340,6 @@ return {
     end,
   },
 
-  { -- Autoformat
-    'stevearc/conform.nvim',
-    event = { 'BufWritePre' },
-    cmd = { 'ConformInfo' },
-    keys = {
-      {
-        '<leader>f',
-        function()
-          require('conform').format { async = true, lsp_format = 'fallback' }
-        end,
-        mode = '',
-        desc = '[F]ormat buffer',
-      },
-    },
-    opts = {
-      notify_on_error = false,
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_format = 'fallback',
-      },
-      formatters = {
-        prettier = {
-          prepend_args = {
-            '--print-width',
-            '100',
-            '--prose-wrap',
-            'always',
-            '--use-tabs',
-            'false',
-            '--tab-width',
-            '2',
-          },
-        },
-      },
-      formatters_by_ft = {
-        lua = { 'stylua' },
-        python = { 'ruff' },
-        json = { 'prettier' },
-        yaml = { 'prettier' },
-        markdown = { 'prettier' },
-      },
-    },
-  },
-
   { -- Autocompletion
     'saghen/blink.cmp',
     event = 'VimEnter',
